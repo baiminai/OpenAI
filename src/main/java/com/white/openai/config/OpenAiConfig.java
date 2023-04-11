27378f1b18c3d707603c2cba6624fd9eafca8d35
文画 <42260274+baiminai@users.noreply.github.com>
2023-04-12 00:23:50 +0800

    Update OpenAiConfig.java

--- src/main/java/com/white/openai/config/OpenAiConfig.java
+++ src/main/java/com/white/openai/config/OpenAiConfig.java
@@ -11,7 +11,7 @@ public class OpenAiConfig {
     public static String URL="https://api.openai.com/v1/chat/completions";
     public static String MODEL= "gpt-3.5-turbo";
 
-    private String KEY ="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";
+    private String KEY ="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";
 
     /*
     * ascasohoa
